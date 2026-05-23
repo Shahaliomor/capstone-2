798a74f7036429c4f57b50c76410424b9bd77b4e
Shah Ali Omor <omoranam@gmail.com>
2026-05-23 17:44:16 -0400

    Add initial UserInterface with home and order screen menus

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,17 @@
 package com.pluralsight;
-
+//Main Class
 public class Main {
+    //Main function
+    public static void main(String[] args) {
+        //Create a UserInterface object
+        UserInterface ui = new UserInterface();
+       //keep application running until user exits.
+        while (true) {
+            ui.displayHomeScreen();
+            System.out.println("Thanks For visiting CHEESY BURG.");
+            break;
+
+        }
+
+    }
 }
--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,40 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class UserInterface {
+    private Order currentOrder;
+    public static Scanner input = new Scanner(System.in);
+    public void displayHomeScreen(){
+        System.out.println("""
+                1) New Order
+                0) Exit - exit the application
+                """);
+
+        int choice = input.nextInt();
+        if(choice == 1){
+            System.out.println("Welcome to CHEESY BURG!");
+        }
+        else if(choice == 0) {
+            return;
+        }
+        else{
+            System.out.println("Invalid choice. Try again.");
+            displayHomeScreen();
+        }
+
+
+    }
+
+    public void displayOrderScreen(){
+        System.out.println("""
+                1) Add Sandwich
+                2) Add Drink
+                3) Add Chips
+                4) Checkout
+                0) Cancel Order - delete the order and go back to the home page
+                """);
+
+    }
+
 }
