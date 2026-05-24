095cee794b1daef4aa72ff85ae6d857f16e04f28
Shah Ali Omor <omoranam@gmail.com>
2026-05-24 15:50:56 -0400

    Add Order class with collections and item management methods.

--- src/main/java/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/Order.java
@@ -6,6 +6,7 @@ public class Order {
     private ArrayList<Sandwich> sandwiches;
     private ArrayList<Drink> drinks;
     private ArrayList<Chips> chips;
+    //Constructor for Order class.
     public Order() {
         this.sandwiches = new ArrayList<>();
         this.drinks = new ArrayList<>();
@@ -18,6 +19,18 @@ public class Order {
 
     public void addSandwich(Sandwich sandwich) {
         this.sandwiches.add(sandwich);
+    }
+    public ArrayList<Drink> getDrinks() {
+        return drinks;
+    }
+    public void addDrink(Drink drink) {
+        this.drinks.add(drink);
+    }
+    public ArrayList<Chips> getChips() {
+        return chips;
+    }
+    public void addChip(Chips chip) {
+        this.chips.add(chip);
     }
 
 }
