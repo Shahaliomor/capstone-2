7e5c12f6f495179bf7f94d32f849a4500f2de0b4
Shah Ali Omor <omoranam@gmail.com>
2026-05-24 04:12:27 -0400

    Add Order class with item collections, constructor, and sandwich management. also started with Sandwitch class

--- src/main/java/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/Order.java
@@ -1,5 +1,23 @@
 package com.pluralsight;
 
-public class Order
-{
+import java.util.ArrayList;
+
+public class Order {
+    private ArrayList<Sandwich> sandwiches;
+    private ArrayList<Drink> drinks;
+    private ArrayList<Chips> chips;
+    public Order() {
+        this.sandwiches = new ArrayList<>();
+        this.drinks = new ArrayList<>();
+        this.chips = new ArrayList<>();
+    }
+
+    public ArrayList<Sandwich> getSandwiches() {
+        return sandwiches;
+    }
+
+    public void addSandwich(Sandwich sandwich) {
+        this.sandwiches.add(sandwich);
+    }
+
 }
--- src/main/java/com/pluralsight/Sandwich.java
+++ src/main/java/com/pluralsight/Sandwich.java
@@ -1,4 +1,11 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Sandwich {
+    private int size;
+    private String bread;
+    private boolean toasted = false;
+    private ArrayList<Topping> toppings;
+    private ArrayList<Sandwich> sandwiches;
 }
