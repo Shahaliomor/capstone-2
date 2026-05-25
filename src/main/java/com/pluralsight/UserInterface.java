86308b6f376b4d4c20f5eda588d9698ebc066633
Shah Ali Omor <omoranam@gmail.com>
2026-05-25 04:20:29 -0400

    Implement sandwich customization flow with size, bread selection, and input validation

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -4,19 +4,23 @@ import java.util.Scanner;
 
 public class UserInterface {
     private int sandwichInch;
+    private String breadType;
     private Order currentOrder;
     private String name;
+
     public static Scanner input = new Scanner(System.in);
-    // Display the home screen menu
-    public void displayHomeScreen(){
+
+    public void displayHomeScreen() {
         System.out.println("""
                 1) New Order
                 0) Exit - exit the application
                 """);
+
         System.out.print("Choose an option: ");
         int choice = input.nextInt();
         input.nextLine();
-        if(choice == 1){
+
+        if (choice == 1) {
             System.out.print("Can I have your name please: ");
             name = input.nextLine();
 
@@ -26,28 +30,27 @@ public class UserInterface {
             System.out.println("How can I help you?");
             displayOrderScreen();
         }
-        else if(choice == 0) {
+        else if (choice == 0) {
             System.out.println("Thanks for choosing CHEESY BURG, Goodbye!");
-            return;
         }
-        else{
-            System.out.println("Invalid choice "+ name+", try again.");
+        else {
+            System.out.println("Invalid choice, try again.");
             displayHomeScreen();
         }
-
-
     }
-    // Show order options
-    public void displayOrderScreen(){
+
+    public void displayOrderScreen() {
         System.out.println("""
                 1) Add Sandwich
                 2) Add Drink
                 3) Add Chips
                 4) Checkout
                 0) Cancel Order - delete the order and go back to the home page
-                  """);
+                """);
+
         System.out.print("Choose an option: ");
         int choice = input.nextInt();
+        input.nextLine();
 
         if (choice == 1) {
             addSandwich();
@@ -66,60 +69,111 @@ public class UserInterface {
             displayHomeScreen();
         }
         else {
-            System.out.println("Invalid choice "+ name+", try again.");
+            System.out.println("Invalid choice " + name + ", try again.");
             displayOrderScreen();
         }
-
     }
-    // Add sandwich to current order
-    public void addSandwich(){
-        while(true){
-            System.out.print("""
-                        How many Inch Sandwich you want to add: 
-                            1) 4"
-                            2) 8"
-                            3) 12"
-                        """);
+
+    public void addSandwich() {
+        while (true) {
+            System.out.println("""
+                    How many inch sandwich do you want to add?
+                    1) 4"
+                    2) 8"
+                    3) 12"
+                    """);
+
             System.out.print("Choose an option: ");
             int choice = input.nextInt();
+            input.nextLine();
+
             if (choice == 1) {
                 sandwichInch = 4;
+                break;
             }
             else if (choice == 2) {
                 sandwichInch = 8;
+                break;
             }
             else if (choice == 3) {
                 sandwichInch = 12;
+                break;
             }
             else {
-                System.out.println("Invalid choice "+ name+". DO you want to try again?");
-                System.out.println("""
-                    1) Yes
-                    2) No
-                    Choose 1 or 2: 
-                    """);
-                int  choice2 = input.nextInt();
-                input.nextLine();
-                if (choice2 == 1) {
+                if (invalidOption()) {
                     continue;
                 }
-                else if (choice2 == 2) {
-                    break;
-                }
                 else {
-                    System.out.println("Invalid choice "+ name);
+                    displayOrderScreen();
                     return;
                 }
             }
         }
 
+        while (true) {
+            System.out.println("""
+                    What kind of bread do you like?
+                    1) White
+                    2) Wheat
+                    3) Rye
+                    4) Wrap
+                    """);
 
+            System.out.print("Choose an option: ");
+            int choice = input.nextInt();
+            input.nextLine();
+
+            if (choice == 1) {
+                breadType = "White";
+                break;
+            }
+            else if (choice == 2) {
+                breadType = "Wheat";
+                break;
+            }
+            else if (choice == 3) {
+                breadType = "Rye";
+                break;
+            }
+            else if (choice == 4) {
+                breadType = "Wrap";
+                break;
+            }
+            else {
+                if (invalidOption()) {
+                    continue;
+                }
+                else {
+                    displayOrderScreen();
+                    return;
+                }
+            }
+        }
+
+        System.out.println("Sandwich selected: " + sandwichInch + "\" " + breadType);
+        displayOrderScreen();
     }
-    // Add drink to current order
-    public void addDrink(){}
-    // Add Chips to current order
-    public void addChips(){}
-    // Display order details and complete order
-    public void checkout(){}
 
+    public void addDrink() {
+    }
+
+    public void addChips() {
+    }
+
+    public void checkout() {
+    }
+
+    public boolean invalidOption() {
+        System.out.println("Invalid choice " + name + ". Do you want to try again?");
+        System.out.println("""
+                1) Yes
+                2) No
+                """);
+
+        System.out.print("Choose 1 or 2: ");
+        int choice = input.nextInt();
+        input.nextLine();
+
+        return choice == 1;
+    }
 }
