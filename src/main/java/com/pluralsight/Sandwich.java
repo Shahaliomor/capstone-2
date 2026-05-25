130dae2beb4e9a7bc6ebc38275033837fb48428a
Shah Ali Omor <omoranam@gmail.com>
2026-05-25 05:54:56 -0400

    Refactor Sandwich class by simplifying structure, adding topping support, and implementing price calculation logic

--- src/main/java/com/pluralsight/Sandwich.java
+++ src/main/java/com/pluralsight/Sandwich.java
@@ -5,27 +5,56 @@ import java.util.ArrayList;
 public class Sandwich implements PriceItem {
     private int size;
     private String bread;
-    private boolean toasted = false;
+    private boolean toasted;
     private ArrayList<Topping> toppings;
-    private ArrayList<Sandwich> sandwiches;
-    private boolean extraToppings = false;
-    private boolean extraCheese = false;
-    private boolean regularToppings = false;
-    private boolean Cheese = false;
-    private String cheese;
-    private ArrayList<Sandwich> extraSandwiches;
-    public Sandwich(int size, String bread,
-                    boolean toasted, boolean extraToppings,
-                    boolean Cheese, boolean extraCheese) {
+
+    public Sandwich(int size, String bread, boolean toasted) {
         this.size = size;
         this.bread = bread;
         this.toasted = toasted;
-        this.sandwiches = new ArrayList<Sandwich>();
-
+        this.toppings = new ArrayList<>();
     }
 
+    public void addTopping(Topping topping) {
+        toppings.add(topping);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getBread() {
+        return bread;
+    }
+
+    public boolean isToasted() {
+        return toasted;
+    }
+
+    public ArrayList<Topping> getToppings() {
+        return toppings;
+    }
+
+    @Override
     public double getPrice() {
         double price = 0;
+
+        if (size == 4) {
+            price = 5.50;
+        }
+        else if (size == 8) {
+            price = 7.00;
+        }
+        else if (size == 12) {
+            price = 8.50;
+        }
+
+        for (Topping topping : toppings) {
+            if (topping instanceof PriceItem) {
+                price += ((PriceItem) topping).getPrice();
+            }
+        }
+
         return price;
     }
 }
