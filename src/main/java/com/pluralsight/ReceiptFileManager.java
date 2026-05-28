940396d94a19bc34bcdff884b39a13df81268dcf
Shah Ali Omor <omoranam@gmail.com>
2026-05-28 01:17:59 -0400

    create ReceiptFileManager and add receipt file writing setup

--- src/main/java/com/pluralsight/ReceiptFileManager.java
+++ src/main/java/com/pluralsight/ReceiptFileManager.java
@@ -1,4 +1,42 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class ReceiptFileManager {
+
+    public static void saveReceipt(Order order) {
+
+        try {
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+
+            String fileName = LocalDateTime.now().format(formatter) + ".txt";
+
+            BufferedWriter writer =
+                    new BufferedWriter(new FileWriter("receipts/" + fileName));
+
+            writer.write("CHEESY BURG RECEIPT");
+            writer.newLine();
+
+            writer.write("Customer: "
+                    + order.getName());
+
+            writer.newLine();
+            writer.newLine();
+
+            writer.write("Order Details:");
+            writer.newLine();
+
+            writer.close();
+
+        }
+        catch (Exception e) {
+            System.out.println(
+                    "Error saving receipt."
+            );
+        }
+    }
 }
